81e7c892e3012007514cd5de3157721b8b3e822d
Benedicte Raae <raae@bgraphic.no>
2017-10-02 14:31:14 +0200

    Fikset velkommen tekst

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
           i samarbeid med
           <img src="~/assets/assistermeg.png"></img>
         </div>
-        <p class="title"> Velkommen til Aktivitetscup for mennesker med psykisk utviklingshemming.
+        <p class="title"> Velkommen til Aktivitetscup for mennesker med utviklingshemming.
         </p>
         <div class="actions">
           <nuxt-link :to="{ name: 'inngang', params: { signup: true }}" class="button is-medium is-primary">
