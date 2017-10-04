2812746f69e64bd8d91dc3763131e96b77845f8a
Ola Vea <ola@olavea.com>
2017-10-04 10:37:20 +0200

    Updated names for photos
    Updated names for photos
    For exMPLE:
    12_trykk_send_inn_IMG_0018.2.jpg
    
    put in underscore to replace "space"

--- pages/info.vue
+++ pages/info.vue
@@ -23,9 +23,9 @@
             <p>Oppskrift 1</p>
             <h2>Meld deg på Aktivitetscupen</h2>
 
-            <img src="/2 Sign up 1 IMG_0003.PNG" alt="Sign up 1 "></img>
+            <img src="/2_Sign_up_1_IMG_0003.PNG" alt="Sign up 1 "></img>
 
-            <img src="/3 Sign up 1 IMG_0004.PNG" alt="Kort beskrivelse av bildet"></img>
+            <img src="/3_Sign_up_1_IMG_0004.PNG" alt="Kort beskrivelse av bildet"></img>
             
             <p>Bruk et passord som er lett å huske, men hvis du ikke logger deg ut av aktivitetscupen så trenger du ikke logge deg inn igjen. Den nye aktivitetscupen husker deg fra gang til gang.</p>
 
@@ -35,9 +35,9 @@
             <h2>Øv med en liksomaktivitet</h2>
             <p>Det er lettere å registrere den første aktiviteten hvis du øver på å registrere en liksomaktivitet.</p>
 
-            <img src="/4 liksomaktivitet 1 IMG_0005_1.jpg" alt="Registrer ny liksomaktivitet 1"></img>
+            <img src="/4_liksomaktivitet_1_IMG_0005_1.jpg" alt="Registrer ny liksomaktivitet 1"></img>
 
-            <img src="/5 liksomaktivitet 2 IMG_0013.2.jpg" alt="5 liksomaktivitet 2"></img>
+            <img src="/5_liksomaktivitet_2_IMG_0013.2.jpg" alt="5 liksomaktivitet 2"></img>
             <p>Ikke trykk "send"</p>
             <p><nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Registerer deg</nuxt-link> </p>
 
@@ -45,23 +45,23 @@
             <h2>Registrer en ekte aktivitet i Aktivitetscupen</h2>
             <p>Registrer en ekte aktivitet, avslutt med å trykke «Send inn».</p>
 
-            <img src="/4 liksomaktivitet 1 IMG_0005_1.jpg" alt="Registrer ny aktivitet"></img>
+            <img src="/4_liksomaktivitet_1_IMG_0005_1.jpg" alt="Registrer ny aktivitet"></img>
 
-            <img src="/7 trykk på trent knappen IMG_0013.3.jpg" alt="trykk på trent knappen"></img>
+            <img src="/7_trykk_på_trent_knappen_IMG_0013.3.jpg" alt="trykk på trent knappen"></img>
 
-            <img src="/8  trykk på egentrening knappen IMG_0014_3.jpg" alt="trykk på egentrening knappen"></img>
+            <img src="/8_trykk_på_egentrening_knappen_IMG_0014_3.jpg" alt="trykk på egentrening knappen"></img>
 
-            <img src="/9 trykk på dato knappen IMG_0015_3.jpg" alt="trykk på dato knappen"></img>
+            <img src="/9_trykk_på_dato_knappen_IMG_0015_3.jpg" alt="trykk på dato knappen"></img>
 
-            <img src="/10 trykk på OK dato knappen IMG_0016.2.jpg" alt="trykk på OK dato knappen"></img>
+            <img src="/10_trykk_på_OK_dato_knappen_IMG_0016.2.jpg" alt="trykk på OK dato knappen"></img>
 
-            <img src="/11 ikke skriv kommentar IMG_0017.2.jpg" alt="ikke skriv kommentar"></img>
+            <img src="/11_ikke_skriv_kommentar_IMG_0017.2.jpg" alt="ikke skriv kommentar"></img>
 
-            <img src="/12 trykk send inn IMG_0018.2.jpg" alt="trykk send inn"></img>
+            <img src="/12_trykk_send_inn_IMG_0018.2.jpg" alt="trykk send inn"></img>
 
             <p><nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Registerer deg</nuxt-link> </p>
 
-            <img src="/Aktivitestcup-Trene.-Telle.-Trene.-Skiforbundblå-300x300.png" alt="aCup"></img>
+            <img src="/1_Aktivitestcup-Trene.-Telle.-Trene.-Skiforbundblå.png" alt="aCup"></img>
 
             <h2>Ny sesong, ny aktivitetscup</h2>
             <p>Hvis du ikke logger deg ut av aktivitetscupen så trenger du ikke logge deg inn igjen. Den nye aktivitetscupen husker deg fra gang til gang og er litt enklere å bruke enn forrige sesong.</p>
