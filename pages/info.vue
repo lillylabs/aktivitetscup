33f7640b1deb8cecbcf908150308ce1d20c419e6
Ola Vea <ola@olavea.com>
2017-10-04 09:26:34 +0200

    Lagt inn knapper
                <p><nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Registerer deg</nuxt-link> </p>
    knappteksten må endres

--- pages/info.vue
+++ pages/info.vue
@@ -25,7 +25,7 @@
             </ol>
 
             <p>Se på bruksanvisningen først eller vær gær'n og bli med uten å se på bruksanvisningen.</p>
-            (-cnapp-)
+            <p><nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Registerer deg</nuxt-link> </p>
 
 
             <p>Oppskrift 1</p>
@@ -37,7 +37,7 @@
             
             <p>Bruk et passord som er lett å huske, men hvis du ikke logger deg ut av aktivitetscupen så trenger du ikke logge deg inn igjen. Den nye aktivitetscupen husker deg fra gang til gang.</p>
 
-            (-cnapp-)&lt;Gå til inngangen: https://aktivitetscup.no/inngang&gt;
+            <p><nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Registerer deg</nuxt-link> </p>
 
             <p>Oppskrift 2</p>
             <h2>Øv med en liksomaktivitet</h2>
@@ -46,6 +46,8 @@
             <img src="/4 liksomaktivitet 1 IMG_0005_1.jpg" alt="Registrer ny liksomaktivitet 1"></img>
 
             <img src="/5 liksomaktivitet 2 IMG_0013.2.jpg" alt="5 liksomaktivitet 2"></img>
+            <p>Ikke trykk "send"</p>
+            <p><nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Registerer deg</nuxt-link> </p>
 
             <p>Oppskrift 3</p>
             <h2>Registrer en ekte aktivitet i Aktivitetscupen</h2>
@@ -65,12 +67,12 @@
 
             <img src="/12 trykk send inn IMG_0018.2.jpg" alt="trykk send inn"></img>
 
-            (-cnapp-)Jeg blir med nå. (-cnapp-)
+            <p><nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Registerer deg</nuxt-link> </p>
 
             <h2>Ny sesong, ny aktivitetscup</h2>
             <p>Hvis du ikke logger deg ut av aktivitetscupen så trenger du ikke logge deg inn igjen. Den nye aktivitetscupen husker deg fra gang til gang og er litt enklere å bruke enn forrige sesong.</p>
 
-            (-cnapp-)Jeg blir med nå. (-cnapp-)
+            <p><nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Registerer deg</nuxt-link> </p>
 
             <h2>Ta kontakt</h2>
             <p>Send ros, spørsmål og problemer til aktivitetscup@gmail.com.</p>
