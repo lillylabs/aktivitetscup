8c59f1d86d14df94201ff911608c62fd81bf0cd9
Ola Vea <ola@olavea.com>
2017-10-04 12:20:18 +0200

    Infoside (#1)
    * created a better infopage

--- pages/info.vue
+++ pages/info.vue
@@ -9,28 +9,73 @@
     </header>
     <section class="section content">
       <div class="container">
-        <h2>Slik fungerer det</h2>
-        <ul>
-          <li>
-            <nuxt-link :to="{ name: 'inngang', params: { signup: true }}">Registerer deg</nuxt-link> som deltager</li>
-          <li>Fyll ut din profil slik at vi vet hvem du er</li>
-          <li>Registerer dine aktiviteter fortløpende</li>
-        </ul>
+      
+        <p>
+          <img src="/1_Aktivitestcup-Trene.-Telle.-Trene.-Skiforbundblå.png" alt="aCup"></img>
+        </p>
 
-        <p>Ved å delta i Aktivitetcup'en blir du automatisk med i konkurransen om å samle flest poeng.</p>
-        <p>På slutten av sesongen blir deltagerne (en kvinne og en mann) med flest poeng premiert.</p>
-        <p>I tillegg vil det dukke opp små konkurranser underveis i sesongen.</p>
+        <h2>Trene. Telle. Trene.</h2>
+  
+        <p>Aktivitetscup gjør aktivisering av en utøver med utviklingshemning LITT lettere.</p>
+      
+        <h2>Bli med på 1-2-3</h2>
+        <ol>
+          <li>Skriv inn eposten din og passordet ditt.</li>
+          <li>Legg inn en aktivitet i Aktivitetscupen.</li>
+          <li>Registrer enda en aktivitet.</li>
+        </ol>
 
-        <h2>Aktivitetcup.no</h2>
-        <p>Dette er første versjon. Vi kommer til å forbedre nettstedet underveis i sesongen.</p>
+        <p>Se på bruksanvisningen først eller vær gær'n og bli med uten å se på bruksanvisningen.</p>
+        <p>
+          <nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Legg inn eposten din</nuxt-link>
+        </p>
 
-        <h2>Nytt av året</h2>
-        <p>I år er systemet forbedret bittelitt. Skjemaene er like, men du vi vet hvem du er fordi du logger inn gjennom dette nettstedet. Vi håper det gjør opplevelsen bedre. Hvis du lar være å logge ut vil nettstedet huske deg fra gang til gang.</p>
-        <p>Du som var med oss i forrige sesong kan vente med å fylle i profilen, så fyller vi den med informasjonen vi allerede har så fort som mulig.</p>
-        <p>En del av informasjonen som kom med e-postene er ikke klart her på aktivitetscup.no enda, men vil komme snart.</p>
+        <h2>Meld deg på Aktivitetscupen</h2>
+        <p>
+          <img src="/2_Sign_up_1_IMG_0003.PNG" alt="Sign up 1 "></img>
+          &nbsp;
+          <img src="/3_Sign_up_1_IMG_0004.PNG" alt="Kort beskrivelse av bildet"></img>
+        </p>
+        <p>Bruk et passord som er lett å huske, men hvis du ikke logger deg ut av aktivitetscupen så trenger du ikke logge deg inn igjen. Den nye aktivitetscupen husker deg fra gang til gang.</p>
 
-        <h2>Kontakt</h2>
-        <p>Har du spørsmål eller problemer send en e-post til aktivitetscup@gmail.com.</p>
+        <p>
+          <nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Registerer deg</nuxt-link>
+        </p>
+
+        <h2>Registrer en aktivitet i Aktivitetscupen</h2>
+
+        <p>
+          <img src="/4_liksomaktivitet_1_IMG_0005_1.jpg" alt="Registrer ny aktivitet"></img>
+          &nbsp;
+          <img src="/7_trykk_på_trent_knappen_IMG_0013.3.jpg" alt="trykk på trent knappen"></img>
+        </p>
+        <p>
+          <img src="/8_trykk_på_egentrening_knappen_IMG_0014_3.jpg" alt="trykk på egentrening knappen"></img>
+          &nbsp;
+          <img src="/9_trykk_på_dato_knappen_IMG_0015_3.jpg" alt="trykk på dato knappen"></img>
+        </p>
+
+        <p>
+          <img src="/10_trykk_på_OK_dato_knappen_IMG_0016.2.jpg" alt="trykk på OK dato knappen"></img>
+          &nbsp;
+          <img src="/11_ikke_skriv_kommentar_IMG_0017.2.jpg" alt="ikke skriv kommentar"></img>
+        </p>
+        <p>
+          <img src="/12_trykk_send_inn_IMG_0018.2.jpg" alt="trykk send inn"></img>
+        </p>
+        <p>
+          <nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Bli med nå</nuxt-link>
+        </p>
+       
+        <h2>Ny sesong, ny aktivitetscup</h2>
+        <p>Hvis du ikke logger deg ut av aktivitetscupen så trenger du ikke logge deg inn igjen. Den nye aktivitetscupen husker deg fra gang til gang. Vi har også fått en deilig ny oversikt over hvilke aktiviteter som er registrert.</p>
+
+        <p>
+          <nuxt-link class="button is-primary is-medium is-outlined" :to="{ name: 'inngang', params: { signup: true }}">Jeg er med!</nuxt-link>
+        </p>
+
+        <h2>Ta kontakt</h2>
+        <p>Send ros, spørsmål og problemer til aktivitetscup@gmail.com.</p>
       </div>
     </section>
   </article>
@@ -40,5 +85,9 @@
 @import "assets/utilities";
 .section {
   background: $white;
+}
+
+img {
+  vertical-align: top;
 }
 </style>
