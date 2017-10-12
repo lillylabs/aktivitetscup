34a9a75ca749391e81f81923764f1afcf17cb380
Benedicte Raae <raae@bgraphic.no>
2017-10-12 16:54:32 +0200

    Removed "les bruksanvisning" fra bottom.

--- components/Bottom.vue
+++ components/Bottom.vue
@@ -6,7 +6,6 @@
           <strong>Aktivitetscup</strong> er laget av
           <a href="http://lillylabs.no">Lilly Labs</a> for
           <a href="https://www.skiforbundet.no/funksjonshemmede/">Skiforbundet FH</a>.
-          <a href="https://aktivitetscup.no/info/">Les bruksanvisning</a>.
         </p>
       </div>
     </div>
